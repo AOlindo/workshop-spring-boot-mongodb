f3c45db94ecbea93dde2e24ff99ff3a6917f082e
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-11-09 16:17:21 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/aOlindo/workshopmongodb/domain/Post.java
+++ src/main/java/com/aOlindo/workshopmongodb/domain/Post.java
@@ -1,13 +1,16 @@
 package com.aOlindo.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.aOlindo.workshopmongodb.dto.AuthorDTO;
+import com.aOlindo.workshopmongodb.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String body;
 
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 
@@ -79,6 +84,13 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -90,5 +102,7 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 }
