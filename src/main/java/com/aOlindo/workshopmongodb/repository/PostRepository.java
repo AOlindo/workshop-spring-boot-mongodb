709f09df485cabfcaeef8afd7188d708e906dfa7
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-11-09 17:29:26 -0300

    Consulta simples com @Query

--- src/main/java/com/aOlindo/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/aOlindo/workshopmongodb/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.aOlindo.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.aOlindo.workshopmongodb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
